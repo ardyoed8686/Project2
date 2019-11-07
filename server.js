c6dc435b0fa930f8f3dfeb9e3abae8c12388523e
Romy Owens <rdoed160@gmail.com>
2019-11-07 15:13:24 -0500

    initialize branch author

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.set("view engine", "handlebars");
 
 
 // Routes
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // test
 app.get('/', function(req,res){
