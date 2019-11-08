8f37f67f1f0f1ca7fdeabc21f27d8f32c268c422
Romy Owens <rdoed160@gmail.com>
2019-11-07 20:05:09 -0500

    update index.handlebars

--- models/Author.js
+++ models/Author.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Author = sequelize.define("Authors", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    name: DataTypes.STRING,
+    // description: DataTypes.TEXT
   });
   return Author;
 };
--- routes/authorApiRoutes.js
+++ routes/authorApiRoutes.js
@@ -8,9 +8,26 @@ module.exports = function(app) {
     });
   });
 
+
   // Create a new author
   app.post("/api/authors", function(req, res) {
+    //   check if author exists in database
+//     db.Author.findOne({}).then(function(dbAuthor){
+// // if author name not found do db.Author.create
+//     })
     db.Author.create(req.body).then(function(dbAuthor) {
+      res.json(dbAuthor);
+    });
+  });
+
+  // Update an author name
+  app.put("/api/authors/update/:id", function(req, res) {
+    db.Author.update({
+        name: req.body.name},
+        {
+        where: {id: req.params.id}
+        }
+    ).then(function(dbAuthor) {
       res.json(dbAuthor);
     });
   });
