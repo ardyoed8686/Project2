3c2c012f4c0abd9e9e5d299c0ad0ee4f55f59e53
Gitana Brazlauskas <Bgitana@hotmail.com>
2019-11-09 10:56:45 -0500

    models tasks.js

--- models/Category.js
+++ models/Category.js
@@ -7,18 +7,22 @@ module.exports = function(sequelize, DataTypes) {
       
       type: DataTypes.STRING,
     });
-    // Category.associate = function(models){
+    
+    // ============================================/
+    Task.associate = function(models){
 
         // This will continue to throw an error as long as post does not exist in the models directory.
 
         // This is associating the Category to the post that is done.
-        // Category.belongsTo(models.Post, {
-        //     foreignKey: {
-        //       allowNull: false
-        //     }
-        //   });
-    // };
+        Task.belongsTo(models.Post, {
+            foreignKey: {
+              allowNull: false
+            }
+          });
+    };
     
+ // ============================================/
+
     return Category;
    
   };
--- models/task.js
+++ models/task.js
@@ -4,5 +4,21 @@ module.exports = function(sequelize, DataTypes) {
     task: DataTypes.STRING,
     complete: DataTypes.BOOLEAN
   });
+
+  // Category.associate = function(models){
+
+        // BelongsTo//
+        
+        
+        This will continue to throw an error as long as post does not exist in the models directory.
+
+        // This is associating the Category to the post that is done.
+        // Category.belongsTo(models.Post, {
+        //     foreignKey: {
+        //       allowNull: false
+        //     }
+        //   });
+    // };
+
   return Task;
 };
