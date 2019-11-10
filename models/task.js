dfda7a2723566ec4d2c3003da8a55c2fa42b0bdb
Gitana Brazlauskas <Bgitana@hotmail.com>
2019-11-10 01:57:39 -0500

    added LOGO folder with Logo and Home Page

--- models/task.js
+++ models/task.js
@@ -4,5 +4,29 @@ module.exports = function(sequelize, DataTypes) {
     task: DataTypes.STRING,
     complete: DataTypes.BOOLEAN
   });
+
+
+  Task.associate = function(models) {
+    // A Task should belong to an Author
+    // A Task can't be created without an Author due to the foreign key constraint
+    Task.belongsTo(models.Author, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+
+
+    // A Task should belong to an Category
+    // A Task can't be created without a Category due to the foreign key constraint
+    Task.belongsTo(models.Category, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  }
+  
   return Task;
+
+ 
 };
+
