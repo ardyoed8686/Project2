6f92c2dd5001c5b8fa17a88f2be1d53166b5c16d
Alex Puello <apuello88@yahoo.com>
2019-11-12 12:08:10 -0500

    added  a post request to add a user, then moved on to check if the user exists in the database when the user signs in.

--- routes/userApiRoutes.js
+++ routes/userApiRoutes.js
@@ -3,11 +3,45 @@ var db = require("../models");
 module.exports = function(app) {
 
 app.get("/api/user", function(req,res){
-    // db.User.findAll({}).then(function(dbUser){
-    //     res.json(dbUser)
-    //    });
-    res.send("ha it works!!");
+    db.User.findAll({where:{}}).then(function(dbUser){
+        res.json(dbUser)
+       });
+    // res.send("ha it works!!");
    
+});
+
+
+// This validates user log in.
+app.get("/api/login_data", function(req,res){
+    db.User.findOne({where:{username:"Gassy", password:"mobius1"}}).then(function(dbUser){
+        
+        if(dbUser === null){
+            res.send("Sorry we have no record of the username/password you entered. Please try again or sign up.");
+
+        }else if (dbUser){
+            res.send("Welcome " + dbUser.username + "!");
+        }
+        console.log(dbUser)
+
+        }); 
+
+    // res.json(dbUser);
+});
+
+
+app.post("/api/login", function(req, res){
+
+var usr = req.body.userLogin;
+var usrpassword = req.body.userPassword
+
+app.get("/api/login_data", function(req,res){
+    db.User.findAll({where:{username:usr, password:usrpassword}}).then(function(dbUser){
+        res.json(dbUser.length)
+       }); 
+});
+
+
+
 });
 
 app.post("/api/user", function(req, res){
